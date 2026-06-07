2a0aaeb667b0cc7fcbbaeef5fe1769fc594f8215
hcsarker <hcsarker2002@gmail.com>
2026-06-07 08:01:32 +0600

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
+  base: "/",
   plugins: [
     tanstackStart({
       server: {
